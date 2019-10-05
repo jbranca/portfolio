13ef4ff4d93cf49a01c5fddc236102fa3c9083e8
John Branca <jbranca@sportingnews.com>
2019-10-05 13:47:21 -0400

    First pass at portfolio site

---
+++ public/js/jobs.js
@@ -0,0 +1,84 @@
+const jobs = [
+  {
+    title: 'Head of Development - US',
+    company: 'DAZN Group (formerly Perform Media)',
+    location: 'Charlotte, NC',
+    dates: 'June 2018 - Present',
+    img: '/img/dazn-sn.png',
+    mobileImg: '/img/dazn-sn-mobile.png',
+    description: [
+      `Developed project scope, timelines, and tasks working closely with the product team using agile methodologies.`,
+      `Helped scrum team to rebuild an additional 3 Sporting News editions (Canada, Australia, and the US) and 2 NBA editions (Japan and Australia)  using JavaScript (ES6), Laravel, Redis and Akamai.`,
+      `Implement A/B tests using Google Optimize and SEO optimizations to improve the overall user engagement among Sporting News’ editions.`,
+      `Managed an eight person team, five developers, two quality assurance testers and one scrum master.`,
+      `Interviewed and hired developers and QA testers, evaluating applicants technically and cultural fit.`
+    ]
+  },
+  {
+    title: 'Head of Development - US',
+    company: 'Perform Media',
+    location: 'Charlotte, NC',
+    dates: 'June 2013 - June 2018',
+    img: '/img/perform-sn.png',
+    mobileImg: '/img/perform-sn.png',
+    description: [
+      `Lead developer on a scrum team that built and released 5 global editions for the NBA and 1 Sporting News edition (Japan) with a JavaScript(ES6), Laravel, Redis and Akamai stack.`,
+      `Created Australian edition of sportingnews.com`,
+      `Architected standards across Sporting News and NBA using Webpack, Babel JS, ESLint and Sass.`,
+      `Added support for Shorthand, including adding tracking by processing files through Amazon S3 and Lambda`
+    ]
+  },
+  {
+    title: 'Web Developer III',
+    company: 'ESPN',
+    location: 'New York, NY',
+    dates: 'August 2012 - June 2013',
+    img: '',
+    description: [
+      `Responsible for the user management of push/SMS notifications in ESPN apps, including Scorecenter via a proprietary language called Tea.`,
+      `Cleaned up the Alerts API for use in all apps by reorganizing the API to be more accessible.`,
+      `Added Windows Phone and Windows 8 alert notifications, including game alerts.`,
+    ]
+  },
+  {
+    title: 'Web Developer promoted to Senior Web Developer',
+    company: 'Sporting News',
+    location: 'New York NY and Charlotte, NC',
+    dates: 'August 2006 - August 2012',
+    img: '',
+    description: [
+      `Released and maintained over 10 salary cap fantasy sports games with over 130,000 unique users on a custom language (Velma), PHP, JavaScript (jQuery) and Oracle.`,
+      `Integrated third-party APIs to support editorial content within the fantasy games.`,
+      `Contributed to the design and development of web applications for various advertising campaigns that utilized a LAMP stack.`,
+      `Converted statistics XML parsers to use MySQL instead of Oracle.`,
+      `Converted database table objects to use Doctrine ORM, an object relational mapper.`
+    ]
+  },
+  {
+    title: 'Web Developer',
+    company: 'New England Research Institutes',
+    location: 'Watertown, MA',
+    dates: 'June 2004 - August 2006',
+    img: '',
+    description: [
+      `Developer for ASP/JavaScript based forms for anonymous medical clinical trial data.  Clients included pharmaceutical companies and the federal government (FDA).`,
+      `Responsibilities for each project included meeting with the project lead, receiving instructions on how each form was to look and function, forwarding on the form to the forms programmers, and interacting between the project lead and forms programmer to complete each form.`
+    ]
+  }
+];
+
+const certifications = [
+  {
+    title: 'Application Security and Secure Coding Training, PHP',
+    company: 'Codebashing by Checkmark'
+  }
+];
+
+const education = [
+  {
+    degree: 'Bachelor of Science',
+    concentration: 'Computer Science',
+    university: 'Villanova University',
+    graduation_date: 'Graduated in 2004'
+  }
+];
---
+++ public/js/resume.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const resumeElement = React.createElement;
+
+class ResumeHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
+  render() {
+    const className = this.state.clicked ? 'pinned' : 'notpinned';
+
+    return (
+      <header className={className} onClick={() => { this.state.clicked = !this.state.clicked } }>
+        <div className="header-name">JOHN BRANCA</div>
+        <div className="header-info">
+          <div><a href="mailto:john.branca@gmail.com">john.branca@gmail.com</a></div>
+          <div><a href="https://github.com/jbranca">https://github.com/jbranca</a></div>
+          <div>(508) 241-8605</div>
+          <div>Charlotte, NC</div>
+        </div>
+      </header>
+    );
+  }
+}
+
+function ResumeJobDescription(props) {
+  return (
+    <li key={props.keyId}>{props.description}</li>
+  );
+}
+
+class ResumeCertifications extends React.Component {
+  render() {
+    let html = '';
+
+    for (var i = 0; i < certifications.length; i++) {
+      html += `${certifications[i].title} - ${certifications[i].company}`;
+    }
+
+    return html;
+  }
+}
+
+function ResumeJob(props) {
+  let jobDescriptions = [];
+  let jobImg = props.job.img;
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth, 
+    window.innerWidth || 0
+  );
+
+  if (viewportWidth < 400) {
+    jobImg = props.job.mobileImg;
+  }
+
+  for (var i = 0; i < props.job.description.length; i++) {
+    jobDescriptions.push(
+      ResumeJobDescription(
+        { 
+          description: props.job.description[i],
+          keyId: props.keyId + '-description-' + i
+        }
+      )
+    );
+  }
+
+  return (
+    <div className="job" key={props.keyId} style={{backgroundImage: 'url(' + jobImg + ')'}}>
+      <div className="job-header">
+        <div className="job-title">
+          {props.job.title} - {props.job.company} - {props.job.location}
+        </div>
+        <div className="job-dates">
+          {props.job.dates}
+        </div>
+      </div>
+      <div className="job-description">
+        <ul>
+          {jobDescriptions}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  let resumeJobs = [];
+
+  for (var i = 0; i < jobs.length; i++) {
+    resumeJobs.push(ResumeJob({ job: jobs[i], keyId: 'job' + i }));
+  }
+
+  return (
+    <div className="resume">
+      <ResumeHeader />
+      <div className="resume-jobs">
+        {resumeJobs}
+      </div>
+      <div className="certifications">
+        <ResumeCertifications />
+      </div>
+      <div className="education">
+
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
